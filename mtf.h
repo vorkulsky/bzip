6011ade8adcd49147dac9311dc880595f2ac05eb
vorkulsky <vorkulsky@gmail.com>
2013-01-30 17:04:26 +0600

    commenting

--- mtf.h
+++ mtf.h
@@ -4,7 +4,16 @@
 
 #include "definitions.h"
 
+/**
+*	block - заполненный массив входных данных. size - его размер.
+*	Результат после выполения будет в block. Размер не изменится.
+*/
 void MTFDecode(byte* block, int size);
+
+/**
+*	block - заполненный массив входных данных. size - его размер.
+*	Результат после выполения будет в block. Размер не изменится.
+*/
 void MTFEncode(byte* block, int size);
 
 #endif
