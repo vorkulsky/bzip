e3bf3592ac1a3374f5234bee91b3fc6423efd197
vorkulsky <vorkulsky@gmail.com>
2013-02-01 15:16:44 +0600

    rename

--- mtf.cpp
+++ mtf.cpp
@@ -8,12 +8,12 @@ void MTFDecode(byte* block, int size)
 	byte counter = 0;
 	for (list<byte>::iterator it = encoding.begin(), end = encoding.end(); it != end; ++it, ++counter)
 		*it = counter;
-	byte current_byte = 0;
+	byte currentByte = 0;
 	for (int i=0; i<size; ++i)
 	{
-		current_byte = block[i];
+		currentByte = block[i];
 		list<byte>::iterator it = encoding.begin();
-		for (byte j=0; j < current_byte; ++j) ++it;
+		for (byte j=0; j < currentByte; ++j) ++it;
 		block[i] = *it;
 		encoding.erase(it);
 		encoding.push_front(block[i]);
@@ -26,17 +26,17 @@ void MTFEncode(byte* block, int size)
 	byte counter = 0;
 	for (list<byte>::iterator it = encoding.begin(), end = encoding.end(); it != end; ++it, ++counter)
 		*it = counter;
-	byte current_byte = 0;
+	byte currentByte = 0;
 	for (int i=0; i<size; ++i)
 	{
-		current_byte = block[i];
+		currentByte = block[i];
 		list<byte>::iterator pos = encoding.begin();
 		byte counter = 0;
 		for (; ;++pos, ++counter)
-			if (*pos == current_byte)
+			if (*pos == currentByte)
 				break;
 		block[i] = counter;
 		encoding.erase(pos);
-		encoding.push_front(current_byte);
+		encoding.push_front(currentByte);
 	}
 }
